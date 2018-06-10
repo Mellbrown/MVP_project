b21b0db5c6adaccf29f5291d633208e9872c1b43
TECLAST\kjc <chan940621@gmail.com>
2018-06-10 12:43:57 +0900

    1. Firebase 연결

--- app/src/main/java/com/techwork/kjc/mvp_project/MainActivity.java
+++ app/src/main/java/com/techwork/kjc/mvp_project/MainActivity.java
@@ -3,7 +3,12 @@ package com.techwork.kjc.mvp_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
