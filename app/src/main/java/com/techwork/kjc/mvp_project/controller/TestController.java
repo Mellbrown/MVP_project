d6e6edab821d8e44d593b167aad41098bfdfda55
deokgoo <kkddgg1001@gmail.com>
2018-06-11 17:42:28 +0900

    ACT3 init

--- app/src/main/java/com/techwork/kjc/mvp_project/controller/TestController.java
+++ app/src/main/java/com/techwork/kjc/mvp_project/controller/TestController.java
@@ -1,12 +1,25 @@
 package com.techwork.kjc.mvp_project.controller;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.techwork.kjc.mvp_project.activity.ACT3_Login;
 
 public class TestController extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, ACT3_Login.class);
+        intent.putExtra(ACT3_Login.REQUEST_LISTNER, new ACT3_Login.RequestListener() {
+            @Override
+            public void onRequestLogin(String email, String password) {
+
+            }
+        });
+        startActivity(intent);
     }
 }
