418a0dbcb487213e3d1c74bc6e77c7c0f18b8dcc
omega3 <2mellbrown@gmail.com>
2018-06-23 10:58:01 +0900

    FRG8 리메이크

---
+++ app/src/main/java/com/techwork/kjc/mvp_project/subview/SubFRG8_Graph.java
@@ -0,0 +1,86 @@
+package com.techwork.kjc.mvp_project.subview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.techwork.kjc.mvp_project.R;
+
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.models.BarModel;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class SubFRG8_Graph extends FrameLayout{
+
+    private View viewLayout;
+
+    private FrameLayout sel_date;
+    private BarChart mBarChart;
+
+    private CusSelDateView cusSelDateView;
+    private CusSelDateView.SimpleDate curDate = new CusSelDateView.SimpleDate();
+
+    private OnRequesterDataOfDate onRequesterDataOfDate;
+
+    public SubFRG8_Graph(@NonNull Context context,  OnRequesterDataOfDate onRequesterDataOfDate) {
+        super(context);
+
+        this.onRequesterDataOfDate = onRequesterDataOfDate;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        viewLayout = inflater.inflate(R.layout.subview_act8_graph, this, false);
+        addView(viewLayout);
+
+        sel_date = viewLayout.findViewById(R.id.sel_date);
+        mBarChart = viewLayout.findViewById(R.id.barchart);
+
+        cusSelDateView = new CusSelDateView(getContext(), curDate, new CusSelDateView.OnChangedDateListener() {
+            @Override
+            public void onChangedDate(CusSelDateView.SimpleDate date) {
+                curDate = date;
+                onRequesterDataOfDate.onRequesterDataOfDate(date);
+            }
+        });
+        sel_date.addView(cusSelDateView, -1, -2);
+
+        onRequesterDataOfDate.onRequesterDataOfDate(curDate);
+    }
+
+    public static class Item {
+        public int number;
+        public int level;
+        public int reps;
+    }
+
+    public void setData(ArrayList<Item> items){
+        Collections.sort(items,(Item o1,Item o2)->o1.level - o2.level);
+        int graphData[] = new int[items.get(items.size()-1).level + 1];
+        Arrays.fill(graphData, 0);
+
+        for(int i = 0; items.size() > i ; i++){
+            Item item = items.get(i);
+            graphData[item.level] += item.reps;
+        }
+
+        ArrayList<BarModel> barModels = new ArrayList<>();
+        for(int i = 0; graphData.length > i; i++){
+            barModels.add(new BarModel(i + ".Lv", graphData[i], Color.YELLOW));
+        }
+
+        mBarChart.clearChart();
+        mBarChart.addBarList(barModels);
+    }
+
+    public interface OnRequesterDataOfDate{
+        void onRequesterDataOfDate(CusSelDateView.SimpleDate requestDate);
+    }
+}
---
+++ app/src/main/java/com/techwork/kjc/mvp_project/subview/SubFRG8_Track.java
@@ -0,0 +1,118 @@
+package com.techwork.kjc.mvp_project.subview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.techwork.kjc.mvp_project.R;
+import com.techwork.kjc.mvp_project.adapter.BaseRecyclerAdapter;
+
+import java.util.ArrayList;
+
+public class SubFRG8_Track extends FrameLayout implements View.OnClickListener {
+
+    private View viewLayout;
+
+    private FrameLayout level_frame;
+    private FrameLayout reps_frame;
+    private FloatingActionButton btnUpload;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private BaseRecyclerAdapter<Item,ViewHolder> recyclerAdapter;
+
+    private CusInputUpdownCounter levelCusInputUpdownCounter;
+    private CusInputUpdownCounter repsCusInputUpdownCounter;
+
+    private int valueLevel = 0;
+    private int valueReps = 0;
+
+    private OnRequesterUploadItem onRequesterUploadItem;
+
+    public SubFRG8_Track(@NonNull Context context, OnRequesterUploadItem onRequesterUploadItem) {
+        super(context);
+        this.onRequesterUploadItem = onRequesterUploadItem;
+        //레이아웃 준비
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        viewLayout = inflater.inflate(R.layout.subview_act8_track, this, false);
+        addView(viewLayout);
+        // Ui
+        level_frame = viewLayout.findViewById(R.id.level_frame);
+        reps_frame = viewLayout.findViewById(R.id.reps_frame);
+        btnUpload = viewLayout.findViewById(R.id.btnUpload);
+        recyclerView = viewLayout.findViewById(R.id.recyclerView);
+        // UI 프레임에 박기
+        levelCusInputUpdownCounter = new CusInputUpdownCounter(getContext(),
+                "강도(Level)", "", 0, (int value)->valueLevel=value);
+        level_frame.addView(levelCusInputUpdownCounter, -2, -2);
+
+        repsCusInputUpdownCounter = new CusInputUpdownCounter(getContext(),
+                "횟수(Reps)", "개", 0, (int value)->valueReps=value);
+        reps_frame.addView(repsCusInputUpdownCounter, -2, -2);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerAdapter = new BaseRecyclerAdapter<Item, ViewHolder>(R.layout.item_focus, ViewHolder.class) {
+            @Override
+            public void dataConvertViewHolder(ViewHolder holder, Item data) {
+                holder.dataBind(data);
+            }
+        };
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        onRequesterUploadItem.onRequestInitdata();
+
+        btnUpload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(valueLevel == 0 && valueReps == 0) return;
+        onRequesterUploadItem.onReqeusteUploadItem(valueLevel, valueReps);
+    }
+
+    public static class Item{
+        public int number;
+        public int level;
+        public int reps;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public View itemView;
+        public TextView act8_item_num;
+        public TextView act8_item_level;
+        public TextView act8_item_reps;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+
+            act8_item_num = itemView.findViewById(R.id.act8_item_num);
+            act8_item_level = itemView.findViewById(R.id.act8_item_level);
+            act8_item_reps = itemView.findViewById(R.id.act8_item_reps);
+        }
+
+        public void dataBind(Item item){
+            act8_item_num.setText(item.number + "");
+            act8_item_level.setText(item.level + "");
+            act8_item_reps.setText(item.reps + "");
+        }
+    }
+
+    public void setData(ArrayList<Item> items){
+        recyclerAdapter.dataList.clear();
+        recyclerAdapter.dataList.addAll(items);
+        recyclerAdapter.notifyDataSetChanged();
+    }
+
+    public interface OnRequesterUploadItem{
+        void onRequestInitdata();
+        void onReqeusteUploadItem(int level, int reps);
+    }
+}
