0e390aa1ef18311cf35e4f9a715bb5b210e23589
omega3 <2mellbrown@gmail.com>
2018-06-23 22:31:57 +0900

    메저 코드 DAO 삽입 (but, 진단서 아직 안함)

--- app/src/main/java/com/techwork/kjc/mvp_project/controller/Third_MeauserController.java
+++ app/src/main/java/com/techwork/kjc/mvp_project/controller/Third_MeauserController.java
@@ -1,6 +1,7 @@
 package com.techwork.kjc.mvp_project.controller;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -8,13 +9,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
 import com.techwork.kjc.mvp_project.dialog.InputMeasureRecordDialog;
+import com.techwork.kjc.mvp_project.dialog.ProgressDialog;
 import com.techwork.kjc.mvp_project.dialog.ShowPreScriptionDialog;
 import com.techwork.kjc.mvp_project.fireSource.Fire_GOS;
 import com.techwork.kjc.mvp_project.fragment.FRG5_Measure;
+import com.techwork.kjc.mvp_project.g2uSubmarineModel.MeasureDAO;
+import com.techwork.kjc.mvp_project.g2uSubmarineModel.UserPhotoDAO;
+import com.techwork.kjc.mvp_project.g2uSubmarineModel.beanse.MeasureBean;
+import com.techwork.kjc.mvp_project.util.EventChain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Third_MeauserController extends AppCompatActivity implements FRG5_Measure.Requester {
 
@@ -52,25 +65,53 @@ public class Third_MeauserController extends AppCompatActivity implements FRG5_M
 
     @Override //화면에서 전체 목록 달라는 요구 들어왔다 그졍?
     public void requestMeasureItemBeans() {
-        // 응답은 비동기적으로 대답해줄수 있습니다.
-        ArrayList<FRG5_Measure.MeasureItemBean> measureItemBeans = new ArrayList<>();
-        for (int i = 0 ; 30 > i ; i++)
-            measureItemBeans.add(new FRG5_Measure.MeasureItemBean(true));
-        //빠베에 데이터 준비 되면 아래 코드로 잡아 넣어주면 됩니다.
-        getFragmentInstance().responseMeasureItemBeans(measureItemBeans);
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        MeasureDAO.selectMeasureBeanseByUID(uid, new MeasureDAO.OnSelectedMeasureBeans() {
+            @Override
+            public void onSelectedMeasureBeans(boolean success, List<MeasureBean> measureBeans, DatabaseError databaseError) {
+                if(success){
+                    ArrayList<FRG5_Measure.MeasureItemBean> measureItemBeans = new ArrayList<>();
+                    for (MeasureBean measureBean : measureBeans){
+                        FRG5_Measure.MeasureItemBean measureItemBean = new FRG5_Measure.MeasureItemBean();
+                        measureItemBean.timestamp = measureBean.timestamp;
+                        measureItemBean.armWeight = measureBean.arm;
+                        measureItemBean.legWeight = measureBean.leg;
+                        measureItemBean.backWeight = measureBean.back;
+                        measureItemBean.allBodyWeight = measureBean.body;
+                        measureItemBeans.add(measureItemBean);
+                    }
+                    getFragmentInstance().responseMeasureItemBeans(measureItemBeans);
+                } else {
+                    Toast.makeText(Third_MeauserController.this, "친구 목록을 불러올수 없습니다.", Toast.LENGTH_SHORT).show();
+                    Log.e("Third_MeauserController", databaseError.getMessage());
+                }
+            }
+        });
     }
 
     @Override // 화면에서 사용자가 + 버튼 눌럿데여! (저긴 버튼 클릭 이벤트 발생 외엔 아무것도 않함;;
     public void requestAddMeasureItem() {
-        // 고로 사용자한테 아이템 받아 오는것도 수동으로 알아서 여기서 받아와야함
-        // 고로 아래 사용자한테 아이템 받아 다이어로그 띄워주면 됨.
         new InputMeasureRecordDialog(Third_MeauserController.this, new InputMeasureRecordDialog.OnSaveListener() {
             @Override // 다이어로그가 사용자한테 값 받아 왔을때 얘기임
             public void onSave(FRG5_Measure.MeasureItemBean measureItemBean) {
-                // 여기다가 이제 빠베 올리는 코드를 짜면 되것정?
-                // 글고 빠베 실패 하면 처리 안하고
-                // 성공하면 아래 코드 실행 시켜 화면에도 삽입 완료됬음을 알려주면 되겠졍?
-                getFragmentInstance().responseAddMeasureItem(measureItemBean);
+                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                MeasureBean measureBean = new MeasureBean();
+                measureBean.timestamp = measureItemBean.timestamp;
+                measureBean.arm = measureItemBean.armWeight;
+                measureBean.leg = measureItemBean.legWeight;
+                measureBean.back = measureItemBean.backWeight;
+                measureBean.body = measureItemBean.allBodyWeight;
+                MeasureDAO.addMeasureBeanWithUID(uid,measureBean).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            getFragmentInstance().responseAddMeasureItem(measureItemBean);
+                        } else {
+                            Toast.makeText(Third_MeauserController.this, "서버에 데이터를 전달하지 못하였습니다.", Toast.LENGTH_SHORT).show();
+                            Log.e("Third_MeauserController", task.getException().getMessage());
+                        }
+                    }
+                });
             }
         }).show();
     }
@@ -93,7 +134,24 @@ public class Third_MeauserController extends AppCompatActivity implements FRG5_M
     public void requestRemoveThisMeasureItem(FRG5_Measure.MeasureItemBean measureItemBean) {
         //화면에서 어떤것만 지울지 선택했지 아무것도 안됬어여
         //여기서 빠베에 지우는 코드 실행시키고 역시 성공하면 응답해주고, 못하면 재까세야
-        getFragmentInstance().responseRemoveSuccessMeasureItem(measureItemBean);
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        MeasureBean measureBean = new MeasureBean();
+        measureBean.timestamp = measureItemBean.timestamp;
+        measureBean.arm = measureItemBean.armWeight;
+        measureBean.leg = measureItemBean.legWeight;
+        measureBean.back = measureItemBean.backWeight;
+        measureBean.body = measureItemBean.allBodyWeight;
+        MeasureDAO.removeMeasureBeanWithUID(uid, measureBean).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    getFragmentInstance().responseRemoveSuccessMeasureItem(measureItemBean);
+                } else {
+                    Toast.makeText(Third_MeauserController.this, "서버에 데이터를 삭제하지 못하였습니다.", Toast.LENGTH_SHORT).show();
+                    Log.e("Third_MeauserController", task.getException().getMessage());
+                }
+            }
+        });
     }
 
     // 현재 프래그먼트 코드 타이핑 치기 귀찮아서 만들었어여
