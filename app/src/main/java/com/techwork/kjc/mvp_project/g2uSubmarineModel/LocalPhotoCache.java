9909466ef280799c56bba3aac96af0bc2809918e
omega3 <2mellbrown@gmail.com>
2018-06-23 15:25:08 +0900

    유저 정보 및 사진(로컬캐싱지원) DAO 객체 제작, (권한 체크 필요)

---
+++ app/src/main/java/com/techwork/kjc/mvp_project/g2uSubmarineModel/LocalPhotoCache.java
@@ -0,0 +1,27 @@
+package com.techwork.kjc.mvp_project.g2uSubmarineModel;
+
+import android.net.Uri;
+import android.os.Environment;
+
+import java.io.File;
+
+public class LocalPhotoCache {
+    public static Uri getCacheFileUri(String prefix, String name){
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "techwork.mvp");
+        if(!storageDir.exists()){
+            storageDir.mkdirs();
+        }
+        File file = new File(storageDir, prefix + "-" +name);
+        Uri uri = Uri.fromFile(file);
+        return uri;
+    }
+
+    public static boolean isExistCacheFile(String prefix, String name){
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "techwork.mvp");
+        if(!storageDir.exists()){
+            storageDir.mkdirs();
+        }
+        File file = new File(storageDir, prefix + "-" +name);
+        return file.exists();
+    }
+}
