7d5eeed50d4597bd315fb819c3e2260d448bc911
omega3 <2mellbrown@gmail.com>
2018-06-26 00:07:36 +0900

    MVP 컨트롤러 만듬

--- app/src/main/java/com/techwork/kjc/mvp_project/controller/Fourth_MVPController.java
+++ app/src/main/java/com/techwork/kjc/mvp_project/controller/Fourth_MVPController.java
@@ -1,25 +1,24 @@
 package com.techwork.kjc.mvp_project.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import com.techwork.kjc.mvp_project.fragment.FRG10_Record;
-import com.techwork.kjc.mvp_project.subview.CusCalView;
+import com.techwork.kjc.mvp_project.fragment.FRG10_MVP;
 
-import java.util.Map;
+import java.util.List;
 
-public class Third_RecordController extends AppCompatActivity implements View.OnClickListener {
+public class Fourth_MVPController extends AppCompatActivity implements FRG10_MVP.Requester {
 
     FragmentManager fragmentManager;
     FrameLayout frameLayout;
     int containerID;
+
+    List<FRG10_MVP.Item> items;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,20 +34,38 @@ public class Third_RecordController extends AppCompatActivity implements View.On
         setContentView(frameLayout);
         fragmentManager = getSupportFragmentManager();
 
-
     }
 
-    private void redering(Map<CusCalView.SimpleDate, FRG10_Record.Item> dataMap){
-        FRG10_Record frg10_record = new FRG10_Record();
-        frg10_record.dataMap =dataMap;
-        frg10_record.onClickListener = this;
+    private void rendering(){
+        FRG10_MVP frg10_mvp = new FRG10_MVP();
+        frg10_mvp.requester = this;
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(containerID, frg10_record,"frg10_record");
+        fragmentTransaction.replace(containerID, frg10_mvp,"frg10_mvp");
         fragmentTransaction.commitAllowingStateLoss();
     }
 
     @Override
-    public void onClick(View v) {
-        startActivity(new Intent(this,Fourth_MVPController.class));
+    public List<FRG10_MVP.Item> requestInitMdataset() {
+        return null;
+    }
+
+    @Override
+    public void requestMdataset() {
+
+    }
+
+    @Override
+    public void requestVdataset() {
+
+    }
+
+    @Override
+    public void requestPdataset() {
+
+    }
+
+    @Override
+    public void reqestShowMVP() {
+
     }
 }
