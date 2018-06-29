4f85ce5799a8d9173576c77590dc30150fe8ab06
omega3 <2mellbrown@gmail.com>
2018-06-30 00:07:25 +0900

    dd

--- app/src/main/java/com/techwork/kjc/mvp_project/fragment/FRG5_Measure.java
+++ app/src/main/java/com/techwork/kjc/mvp_project/fragment/FRG5_Measure.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.Exclude;
@@ -34,8 +36,8 @@ import java.util.Date;
 
 public class FRG5_Measure extends Fragment implements View.OnClickListener {
 
-    private FloatingActionButton btnAdd;
-    private FloatingActionButton btnRemove;
+    private TextView btnAdd;
+    private TextView btnRemove;
     private FloatingActionButton btnReload;
     private FloatingActionButton btnCancle;
 
